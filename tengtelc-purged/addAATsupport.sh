57e7500af95f9dfd16a9caba0d6b3565ff7acc07
machsna <j_mach_wust@shared-files.de>
2021-03-05 19:20:52 +0100

    purging files not needed for tengtelc-style TTF generation; adding beta.tex

--- tengtelc-purged/addAATsupport.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-CpMac tengtelc.ttf tengtelc-incompleteAAT.ttf
-CpMac tengtelcb.ttf tengtelcb-incompleteAAT.ttf
-ftxenhancer -m tengtelc-AAT.mif tengtelc-incompleteAAT.ttf
-ftxenhancer -m tengtelc-AAT.mif tengtelcb-incompleteAAT.ttf
-
